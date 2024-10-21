fd3591df6c6520bfc63ae61b36ab29100f16210f
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-10-21 12:03:48 -0500

    created basic form to enter service and submit drop

--- src/components/Drop.jsx
+++ src/components/Drop.jsx
@@ -5,7 +5,23 @@ export default function Drop() {
     <article className="pageSetup">
       <h1>Drop</h1>
       <p>the main section where the user will enter their daily drop</p>
-      <TattooCard />
+      <form className="dropForm">
+        <label className="labelName">Date:</label>
+        <input
+          className="dropFormInput"
+          type="date"
+          name="drop_date"
+          aria-label="drop_date"
+        />
+        <TattooCard />
+        <input
+          className="dropFormInput"
+          id="submitDrop"
+          type="submit"
+          value="Submit Drop"
+          aria-label="form_submit"
+        />
+      </form>
     </article>
   );
 }
--- src/components/TattooCard.jsx
+++ src/components/TattooCard.jsx
@@ -1,11 +1,59 @@
 export default function TattooCard() {
   return (
-    <article>
-      <h1>Tattoo Card</h1>
-      <p>
-        component placed in the drop component for the user to enter individual
-        services for the day
-      </p>
+    <article className="pageSetup">
+      <h1>Service</h1>
+      <form className="serviceForm">
+        <label className="labelName">Description: </label>
+        <input
+          className="serviceFormInput"
+          type="text"
+          name="user_service"
+          aria-label="user_service"
+        />
+        <div>
+          <label className="labelName">Cash: </label>
+          <input
+            className="serviceFormInput"
+            id="cashInput"
+            type="number"
+            name="service_cash"
+            aria-label="service_cash"
+          />
+          <label className="labelName">Credit: </label>
+          <input
+            className="serviceFormInput"
+            id="creditInput"
+            type="number"
+            name="service_credit"
+            aria-label="service_credit"
+          />
+        </div>
+        <div>
+          <label className="labelName">Deposit: </label>
+          <input
+            className="serviceFormInput"
+            id="depositInput"
+            type="number"
+            name="service_deposit"
+            aria-label="service_deposit"
+          />
+          <label className="labelName">Gift Certificate: </label>
+          <input
+            className="serviceFormInput"
+            id="giftcertInput"
+            type="number"
+            name="service_giftcert"
+            aria-label="service_giftcert"
+          />
+        </div>
+        <input
+          className="serviceFormInput"
+          id="addService"
+          type="submit"
+          value="Add Service"
+          aria-label="form_submit"
+        />
+      </form>
     </article>
   );
 }
