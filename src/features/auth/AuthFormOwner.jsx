ff58e7e1f29a58d3d19b04b3df241adb5a78a0d5
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-10-22 20:00:29 -0500

    created auth folder with owner and team member login in features

---
+++ src/features/auth/AuthFormOwner.jsx
@@ -0,0 +1,98 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useLoginMutation, useRegisterMutation } from "./authSlice";
+
+import "./auth.css";
+
+/** This form allows users to register or log in. */
+export default function AuthFormOwner() {
+  const navigate = useNavigate();
+
+  // Handles swapping between login and register
+  const [isLogin, setIsLogin] = useState(true);
+  const authAction = isLogin ? "Login" : "Register";
+  const altCopy = isLogin
+    ? "Need an account? Register here."
+    : "Already have an account? Login here.";
+
+  // Controlled form fields
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  // Form submission
+  const [login, { isLoading: loginLoading, error: loginError }] =
+    useLoginMutation();
+  const [register, { isLoading: registerLoading, error: registerError }] =
+    useRegisterMutation();
+
+  /** Send the requested authentication action to the API */
+  const attemptAuth = async (evt) => {
+    evt.preventDefault();
+
+    const authMethod = isLogin ? login : register;
+    const credentials = { name, username, password };
+
+    // We don't want to navigate if there's an error.
+    // `unwrap` will throw an error if there is one
+    // so we can use a try/catch to handle it.
+    try {
+      await authMethod(credentials).unwrap();
+      //set login naviagtion to proper paths
+      isLogin ? navigate("/account/") : navigate("/topics/");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  //create a login in form that reflects user and owner tables
+  return (
+    <article className="authForm">
+      <h1>Owner Portal</h1>
+      <h2 id="loginTitle">{authAction}</h2>
+      <form className="form" onSubmit={attemptAuth}>
+        <div id="authForm">
+          <label className="formName">
+            Name
+            <input
+              className="input"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              autoComplete="name"
+            />
+          </label>
+          <label className="formName">
+            Username
+            <input
+              className="input"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              autoComplete="username"
+            />
+          </label>
+          <label className="formName">
+            Password
+            <input
+              className="input"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              autoComplete="current-password"
+            />
+          </label>
+        </div>
+        <button id="loginButton">{authAction}</button>
+      </form>
+      <a id="bottomMessage" onClick={() => setIsLogin(!isLogin)}>
+        {altCopy}
+      </a>
+
+      {(loginLoading || registerLoading) && (
+        <p id="waitMessage">Please wait...</p>
+      )}
+      {loginError && <p role="alert">{loginError}</p>}
+      {registerError && <p role="alert">{registerError}</p>}
+    </article>
+  );
+}
