7cd746e3640486cd5a3241f2e91cdbb879963a41
Richard Marafioti <marafioti@MacBook-Air.local>
2024-10-24 09:29:32 -0500

    created onboarding files for member and owner

---
+++ src/features/members/OnboardMember.jsx
@@ -0,0 +1,13 @@
+export default function OnboardMember() {
+  return (
+    <article>
+      <h1>Enter you contact info to connect with other team members</h1>
+      <form>
+        <label>Phone:</label>
+        <input></input>
+        <label>Email:</label>
+        <input></input>
+      </form>
+    </article>
+  );
+}
---
+++ src/features/owner/OnboardOwner.jsx
@@ -0,0 +1,13 @@
+export default function OnboardOwner() {
+  return (
+    <article>
+      <h1>Create your business</h1>
+      <form>
+        <label>Business Name:</label>
+        <input></input>
+        <label>Business Code:</label>
+        <input></input>
+      </form>
+    </article>
+  );
+}
