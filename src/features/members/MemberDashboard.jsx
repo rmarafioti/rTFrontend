ee8c66644cbb7fe2e8dd3cf19ff5f156aa1935e6
Richard Marafioti <marafioti@MacBook-Air.local>
2024-11-06 14:08:30 -0600

    wrote comments for dashboard functionality and set up

--- src/features/members/MemberDashboard.jsx
+++ src/features/members/MemberDashboard.jsx
@@ -34,13 +34,27 @@ export default function MemberDashboard() {
     return (
       <section>
         <h2>Hello, {member.memberName}</h2>
+        {/* delete phone and email from datebase model because we will build a messaging system */}
         <p>Phone: {member.phone}</p>
         <p>Email: {member.email}</p>
         <p>Your are a member of the {member.business?.businessName} team</p>
         <p>Your Business Code: {member.business?.code}</p>
-        <p>Percentage: {member.percentage}</p>
+        {/* list out all team members linked to the business and map them out send them a message THIS IS A STRETCH GOAL */}
+        <p>Your Team Members:</p>
+        <p>Percentage: {member.percentage} / 40</p>
+
+        {/* break this up here and create another section called DropCard*/}
+
+        {/* list out the date of all of the drop where paid = false */}
+        {/* make each drop date an active link to naviagate to /memberdrop where me will get drop by ID and all services for the drop*/}
+        <p>Current Drops:</p>
+        {/* take home total should add all member totals of every drop*/}
         <p>Take Home Total: {member.takeHomeTotal}</p>
+        {/* you owe total should be all memberOwe totals -  all businessOwe of every drop* if paid === no*/}
+        {/* include a button to pay balance if int is > 0 : state "you have no balance at this time*/}
         <p>You Owe: {member.totalOwed}</p>
+        {/* owe to you total should be all businessOwe totals -  all memberOwe of every drop if paid === no*/}
+        {/* if owe to you int is > 0 else state "business to pay you" : "you are not owed anything at this time"*/}
         <p>Owed to You: {member.totalOwe}</p>
       </section>
     );
@@ -51,12 +65,12 @@ export default function MemberDashboard() {
       <h1>Member Dashboard</h1>
       <MemberCard />
       <button onClick={dropCreateSubmit}>Create a drop</button>
+      {/* list drop totals for each month / functionality can be a separate component STRETCH GOAL */}
       <h3>Monthly Totals: *list totals*</h3>
-      <h3>Current Drops: *list by date / link to individul drop's page*</h3>
+
+      {/* edit drop functionality will be located on the member drop page instead of here */}
       <Link to={`/membereditdrops`}>Edit Drops</Link>
-      <h3>You Currently Owe: *balance to pay*</h3>
-      <Link to={`/memberhandledrops`}>Pay Balance</Link>
-      <h3>Owed to You: *balance business owes*</h3>
+      {/* active link to navigate a team member to all drops where paid = true*/}
       <Link to={`/memberarchive`}>Your Archived Drops</Link>
     </article>
   );
