9ebe1631cd9376ed8b6e574ed176722f9126b7c6
Richard Marafioti <richmarafioti@gmail.com>
2025-02-13 09:55:22 -0600

    added heroku backend to base url to connect deployed frontend

--- src/store/api.js
+++ src/store/api.js
@@ -3,7 +3,10 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3000/api", // Replace with your backend URL and port
+    baseUrl:
+      process.env.NODE_ENV === "production"
+        ? "https://rtbackend-9d8f08c0e69e.herokuapp.com/api"
+        : "http://localhost:3000/api",
     prepareHeaders: (headers, { getState }) => {
       const ownerToken = getState().authOwner?.token;
       const memberToken = getState().authMember?.token;
